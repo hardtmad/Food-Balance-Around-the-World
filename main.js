69b078352cfbb31f1389b5d4b721b3108f70ea58
Alex French <frenchal17@grinnell.edu>
2017-04-28 11:58:37 -0500

    Working on creating an adaptable color gradient for world map; need to convert 3-index-arrays back to strings

--- main.js
+++ main.js
@@ -12,7 +12,38 @@ var svg_height = window.innerHeight;
 // Use d3's built in projection object 
 var projection = d3.geoMercator();
 var path = d3.geoPath().projection(projection);
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+//Citation: Interpolate color functions taken from
+//https://graphicdesign.stackexchange.com/questions/83866/generating-a-series-of-colors-between-two-colors
+var interpolateColor = function (color1, color2, factor) {
+    if (arguments.length < 3) { 
+        factor = 0.5; 
+    }
+    var result = color1.slice();
+    for (var i = 0; i < 3; i++) {
+        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+    }
+    return result;
+};
+
+var interpolateColors = function (color1, color2, steps) {
+    var stepFactor = 1 / (steps - 1),
+        interpolatedColorArray = [];
+
+    color1 = color1.match(/\d+/g).map(Number);
+    color2 = color2.match(/\d+/g).map(Number);
+
+    for(var i = 0; i < steps; i++) {
+        interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
+    }
+
+    return interpolatedColorArray;
+}
+
+var colorSet = interpolateColors("rgb(138, 255, 132)", "rgb(0, 56, 0)", 25);
+console.log(colorSet);
+
+var color = d3.scaleOrdinal(colorSet);
 
 // Generate an SVG element on the page
 var svg = d3.select("body").append("svg")
@@ -45,7 +76,7 @@ d3.json('world-110m.json', function(error, world) {
       	.attr('d', path)
       	.style('fill', function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
 
-  // Create state boundaries
+  // Create country boundaries
   svg.append('path')
     .datum(boundaries)
     .attr('class', 'state-boundary')
