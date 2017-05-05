640a95c8b754f18e050ae08239e2abadde77c564
M Hardt <hardtmad@grinnell.edu>
2017-05-05 12:07:08 -0500

    Clean up and comment code

--- main.js
+++ main.js
@@ -1,160 +1,150 @@
 // Pull in FAO data 
 d3.csv("2013.csv", function(sample) {
   d3.tsv("world-country-names.tsv", function(country_names) {
+    // Use crossfilter for FAO data
+    var amounts = crossfilter(sample);
+    // Make a dimension with the Country field, group by Element and Country
+    var countryDim = amounts.dimension(function (d) { 
+                                        var thisElement = d.Element;
+                                        return 'Element='+thisElement+';Country='+d.Country; } );
+    // Sum all production and supply values for each country
+    var ElementCountry = (countryDim.group().reduceSum(function(d) 
+        {
+          if (d.Element == "Domestic supply quantity")  
+            { 
+              return d.Value}
+          else 
+            { 
+              return d.Value}
+        } ).all());
 
-// Use crossfilter for FAO data
-  var amounts = crossfilter(sample);
-  // Make a dimension with the Country field, group by Element and Country
-  var countryDim = amounts.dimension(function (d) { 
-                                      var thisElement = d.Element;
-                                      return 'Element='+thisElement+';Country='+d.Country; } );
-  // Sum all values for each country, adding for production and subtracting for domestic supply
-  // NEEDS ATTENTION: Fix this formula to be ((production/domestic supply quantity)/100)
+    // Score each country with the formula (prodcution/domestic supply)*100
+    var formulaResult = [];
+    for(i=0; i<175; i++) {
+      var currentDict = {};
+      currentDict.key = ElementCountry[i].key.replace("Element=Domestic supply quantity;Country=", "");
+      currentDict.value = ElementCountry[i+175].value*100/ElementCountry[i].value;
+      formulaResult.push(currentDict);
+      }
 
-  var ElementCountry = (countryDim.group().reduceSum(function(d) 
+    // Map code taken from datavis-interactive lab
+    // Set up the SVG
+    var svg_width = window.innerWidth;
+    var svg_height = window.innerHeight;
+
+    // Use d3's built in projection object 
+    var projection = d3.geoMercator();
+    var path = d3.geoPath().projection(projection);
+
+    //Citation: Interpolate color functions taken from
+    //https://graphicdesign.stackexchange.com/questions/83866/generating-a-series-of-colors-between-two-colors
+    var interpolateColor = function (color1, color2, factor) {
+        if (arguments.length < 3) { 
+            factor = 0.5; 
+        }
+        var result = color1.slice();
+        for (var i = 0; i < 3; i++) {
+            result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+        }
+        return result;
+    };
+
+    var interpolateColors = function (color1, color2, steps) {
+        var stepFactor = 1 / (steps - 1),
+            interpolatedColorArray = [];
+
+        color1 = color1.match(/\d+/g).map(Number);
+        color2 = color2.match(/\d+/g).map(Number);
+
+        for(var i = 0; i < steps; i++) {
+            interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
+        }
+
+        return interpolatedColorArray;
+    }
+
+    var colorSet = interpolateColors("rgb(138, 255, 132)", "rgb(0, 56, 0)", 25);
+
+    for (var j = 0; j < 25; j++) {
+      colorSet[j] = "rgb(" + colorSet[j][0] + "," + colorSet[j][1] + "," + colorSet[j][2] + ")";
+    }
+
+    var color = d3.scaleOrdinal(colorSet);
+
+    // Generate an SVG element on the page
+    var svg = d3.select("body").append("svg")
+        .attr("width", svg_width)
+        .attr("height", svg_height);
+
+    d3.json('world-110m.json', function(error, world) {
+      // Decode the topojson file
+      var land = topojson.feature(world, world.objects.land);
+      var boundaries = topojson.mesh(world, world.objects.countries);
+      var countries = topojson.feature(world, world.objects.countries).features;
+      console.log (countries);
+      var neighbors = topojson.neighbors(world.objects.countries.geometries);
+      // Fit our projection so it fills the window
+      projection.fitSize([svg_width, svg_height], land);
+
+      // Loop over countries to match map ids with names and names with sufficiency scores
+      for (country of countries)
       {
-        if (d.Element == "Domestic supply quantity")  
-          { 
-            return d.Value}
+        country.name = find_name(country.id);
+        current_suff = find_suff(country.name)
+        if (!current_suff)
+          country.suff = 0;
         else 
-          { 
-            return d.Value}
-      } ).all());
+          country.suff = current_suff;
+      }
 
-  console.log(ElementCountry);
+      // Helper function to find id in country_names
+      function find_name (id) {
+        for (entry of country_names) 
+        {
+          if (entry.id == id)
+            return entry.name;
+        }
+      }
 
-  var formulaResult = [];
-  for(i=0; i<175; i++) {
-    var currentDict = {};
-    currentDict.key = ElementCountry[i].key.replace("Element=Domestic supply quantity;Country=", "");
-    currentDict.value = ElementCountry[i+175].value*100/ElementCountry[i].value;
-    formulaResult.push(currentDict);
-  }
-  console.log(formulaResult);
+      // Helper function to find sufficiency given country name
+      function find_suff (name) {
+        for (suff of formulaResult) 
+        {
+          if (name == suff.key)
+            return suff.value;
+        }
+      }
+      
+      // Create land area
+      svg.append('path')
+        .datum(land)
+        .attr('d', path);
 
-// Map code taken from datavis-interactive lab
-
-// Set up the SVG
-var svg_width = window.innerWidth;
-var svg_height = window.innerHeight;
-
-// Use d3's built in projection object 
-var projection = d3.geoMercator();
-var path = d3.geoPath().projection(projection);
-
-//Citation: Interpolate color functions taken from
-//https://graphicdesign.stackexchange.com/questions/83866/generating-a-series-of-colors-between-two-colors
-var interpolateColor = function (color1, color2, factor) {
-    if (arguments.length < 3) { 
-        factor = 0.5; 
-    }
-    var result = color1.slice();
-    for (var i = 0; i < 3; i++) {
-        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
-    }
-    return result;
-};
-
-var interpolateColors = function (color1, color2, steps) {
-    var stepFactor = 1 / (steps - 1),
-        interpolatedColorArray = [];
-
-    color1 = color1.match(/\d+/g).map(Number);
-    color2 = color2.match(/\d+/g).map(Number);
-
-    for(var i = 0; i < steps; i++) {
-        interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
-    }
-
-    return interpolatedColorArray;
-}
-
-var colorSet = interpolateColors("rgb(138, 255, 132)", "rgb(0, 56, 0)", 25);
-
-for (var j = 0; j < 25; j++) {
-  colorSet[j] = "rgb(" + colorSet[j][0] + "," + colorSet[j][1] + "," + colorSet[j][2] + ")";
-}
-
-var color = d3.scaleOrdinal(colorSet);
-
-// Generate an SVG element on the page
-var svg = d3.select("body").append("svg")
-    .attr("width", svg_width)
-    .attr("height", svg_height);
-
-d3.json('world-110m.json', function(error, world) {
-  // Decode the topojson file
-  var land = topojson.feature(world, world.objects.land);
-  var boundaries = topojson.mesh(world, world.objects.countries);
-  var countries = topojson.feature(world, world.objects.countries).features;
-  console.log (countries);
-  var neighbors = topojson.neighbors(world.objects.countries.geometries);
-  // Fit our projection so it fills the window
-  projection.fitSize([svg_width, svg_height], land);
-
-  // Loop over countries to match map ids with naes
-  for (country of countries)
-  {
-    country.name = find_name(country.id);
-    current_suff = find_suff(country.name)
-    if (!current_suff)
-    {
-      country.suff = 0;
-    }
-    else 
-      country.suff = current_suff;
-    console.log(country.name)
-    console.log(country.suff)
-  }
-
-  // Function to find id in country_names
-  function find_name (id) {
-    for (entry of country_names) 
-    {
-      if (entry.id == id)
-        return entry.name;
-    }
-  }
-
-  // Function to find sufficiency given country name
-  function find_suff (name) {
-    for (suff of formulaResult) 
-    {
-      if (name == suff.key)
-        return suff.value;
-    }
-  }
-  
-  // Create land area
-  svg.append('path')
-    .datum(land)
-     //This is the property that sets map-wide color.
-			 //Need to figure out how to do this on a country
-			 //basis.
-    .attr('d', path);
-
-  //Fill in countries by distinct colors
-  svg.selectAll('.country')
-      .data(countries)
-      .enter()
-      .append('path')
-      	.attr('class', 'country')
-      	.attr('d', path)
-      	.style('fill', function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); })
-      	.style('stroke', '#fff')
-      	.on("mouseover", function () {
-      		this.parentNode.appendChild(this);
-    		d3.select(this)
-    		  .style('stroke', '#000');
-    	})
-    	.on("mouseout", function() {
-    	    d3.select(this)
-    		  .style('stroke', '#fff');
-    	})
-      .on("click", function(d) {
-        d3.select(this)
-        console.log(d.name);
-      });
-   });
-});
-});
+      //Fill in countries by distinct colors
+      svg.selectAll('.country')
+          .data(countries)
+          .enter()
+          .append('path')
+          	.attr('class', 'country')
+          	.attr('d', path)
+          	.style('fill', function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); })
+          	.style('stroke', '#fff')
+            // Embolden outline on mouseover
+          	.on("mouseover", function () {
+          		this.parentNode.appendChild(this);
+        		d3.select(this)
+        		  .style('stroke', '#000');
+        	})
+          // Remove outline on mouseout
+        	.on("mouseout", function() {
+        	    d3.select(this)
+        		  .style('stroke', '#fff');
+        	})
+          .on("click", function(d) {
+            d3.select(this)
+            console.log(d.name);
+          });
+          
+       });// end d3.json
+  }); // end d3.tsv
+}); // end d3.csv
