ae7d68ace6401493895933b27e2a0a6bf0d6deab
M Hardt <hardtmad@grinnell.edu>
2017-05-06 13:21:54 -0500

    Refact code to make it easier to display by year

--- main.js
+++ main.js
@@ -1,173 +1,176 @@
-// Pull in FAO data 
-d3.csv("2013.csv", function(sample) {
-  d3.tsv("world-country-names.tsv", function(country_names) {
-
+// Data to match map ids with country names
+ d3.tsv("world-country-names.tsv", function(country_names) {
   /*  ----------------------
-      ---------KEY---------
+      ---------MAP---------
       ---------------------- */
-    // Adjust div with key
-    var d = document.getElementById('key');
+  // Map code taken from datavis-interactive lab http://www.cs.grinnell.edu/~curtsinger/teaching/2017S/CSC395/labs/13.interactive.html
+
+  // Set up the SVG
+  var svg_width = window.innerWidth - 300;
+  var svg_height = window.innerHeight - 40;
+
+  // Use d3's built in projection object 
+  var projection = d3.geoEquirectangular();
+  var path = d3.geoPath().projection(projection);
+
+  // Calculate color set used to color countries
+  var colorSet = interpolateColors("rgb(138, 255, 132)", "rgb(0, 56, 0)", 25);
+
+  for (var j = 0; j < 25; j++) {
+    colorSet[j] = "rgb(" + colorSet[j][0] + "," + colorSet[j][1] + "," + colorSet[j][2] + ")";
+  }
+
+  // Generate an SVG element on the page
+  var svg = d3.select("body").append("svg")
+      .attr("width", svg_width)
+      .attr("height", svg_height)
+      .style("display", "block")
+      .style("margin", "auto");
+
+  // Render world map 
+  d3.json('world-110m.json', function(error, world) {
+    // Decode the topojson file
+    var land = topojson.feature(world, world.objects.land);
+    var countries = topojson.feature(world, world.objects.countries).features;
+    var neighbors = topojson.neighbors(world.objects.countries.geometries);
+    // Fit our projection so it fills the window
+    projection.fitSize([svg_width, svg_height], land);
+
+    // Loop over countries to match map ids with names 
+        for (country of countries)
+        {
+          country.name = find_name(country.id, country_names);
+        }
+
+    /*  ----------------------
+      ---------LEGEND---------
+      ---------------------- */
+    // Adjust div with legend information
+    var d = document.getElementById('legend');
     d.style.position = "absolute";
     d.style.left = 10 + 'px';
     d.style.top = 280 + 'px';
     d.style.height = 300 + 'px';
     d.style.width = 220 + 'px';
 
-  /*  ----------------------
-      ---------DATA---------
-      ---------------------- */
+  // Pull in FAO data 
+  d3.csv("2013.csv", function(sample) {
 
-    // Use crossfilter for FAO data
-    var amounts = crossfilter(sample);
-    // Make a dimension with the Country field, group by Element and Country
-    var countryDim = amounts.dimension(function (d) { 
-                                        var thisElement = d.Element;
-                                        return 'Element='+thisElement+';Country='+d.Country; } );
+    /*  ----------------------
+        ---------DATA---------
+        ---------------------- */
 
-     console.log(sample)
+      // Use crossfilter for FAO data
+      var amounts = crossfilter(sample);
+      // Make a dimension with the Country field, group by Element and Country
+      var countryDim = amounts.dimension(function (d) { 
+                                          var thisElement = d.Element;
+                                          return 'Element='+thisElement+';Country='+d.Country; } );
 
-    // Sum all production and supply values for each country
-    var ElementCountry = (countryDim.group().reduceSum(function(d) 
-        {
-          if (d.Element == "Domestic supply quantity")  
-            { 
-              return d.Value}
-          else 
-            { 
-              return d.Value}
-        } ).all());
-    // Score each country with the formula (prodcution/domestic supply)*100
-    var formulaResult = [];
-    for(i=0; i<175; i++) {
-      var currentDict = {};
-      // Change key to be country name
-      currentDict.key = ElementCountry[i].key.replace("Element=Domestic supply quantity;Country=", "");
-      // Calculate sufficiency score
-      currentDict.value = ElementCountry[i+175].value*100/ElementCountry[i].value;
-      formulaResult.push(currentDict);
-    }
-	
-  /*  ----------------------
-      ---------MAP---------
-      ---------------------- */
+     // Sum all production and supply values for each country
+      var ElementCountry = (countryDim.group().reduceSum(function(d) 
+          {
+            if (d.Element == "Domestic supply quantity")  
+              { 
+                return d.Value}
+            else 
+              { 
+                return d.Value}
+          } ).all());
 
-  // Map code taken from datavis-interactive lab
+      // Score each country with the formula (prodcution/domestic supply)*100
+      var formulaResult = [];
+      for(i=0; i<175; i++) {
+        var currentDict = {};
+        // Change key to be country name
+        currentDict.key = ElementCountry[i].key.replace("Element=Domestic supply quantity;Country=", "");
+        // Calculate sufficiency score
+        currentDict.value = ElementCountry[i+175].value*100/ElementCountry[i].value;
+        formulaResult.push(currentDict);
+      }
 
-	// Set up the SVG
-	var svg_width = window.innerWidth - 300;
-	var svg_height = window.innerHeight - 40;
+    // Loop over countries to match map  names with sufficiency scores
+          for (country of countries)
+          {
+            current_suff = find_suff(country.name, formulaResult)
+            if (current_suff)
+              country.suff = current_suff;
+            else 
+              country.suff = 0;
+        }
 
-	// Use d3's built in projection object 
-	var projection = d3.geoEquirectangular();
-	var path = d3.geoPath().projection(projection);
-
-  // Calculate color set 
-	var colorSet = interpolateColors("rgb(138, 255, 132)", "rgb(0, 56, 0)", 25);
-
-	for (var j = 0; j < 25; j++) {
-	  colorSet[j] = "rgb(" + colorSet[j][0] + "," + colorSet[j][1] + "," + colorSet[j][2] + ")";
-	}
-
-	// Generate an SVG element on the page
-	var svg = d3.select("body").append("svg")
-	    .attr("width", svg_width)
-	    .attr("height", svg_height)
-	    .style("display", "block")
-	    .style("margin", "auto");
-
-	d3.json('world-110m.json', function(error, world) {
-		// Decode the topojson file
-		var land = topojson.feature(world, world.objects.land);
-		var countries = topojson.feature(world, world.objects.countries).features;
-		var neighbors = topojson.neighbors(world.objects.countries.geometries);
-		// Fit our projection so it fills the window
-		projection.fitSize([svg_width, svg_height], land);
-
-		// Loop over countries to match map ids with names and names with sufficiency scores
-      	for (country of countries)
-      	{
-      		country.name = find_name(country.id, country_names);
-	        current_suff = find_suff(country.name, formulaResult)
-	        if (current_suff)
-	          country.suff = current_suff;
-	        else 
-	          country.suff = 0;
-    	}
-
-    	// Scale color set 
-    	var minSuff = d3.min(countries, function (d) { return d.suff });
-    	var maxSuff = d3.max(countries, function (d) { return d.suff });
-		var color = d3.scaleLinear()
-					  .domain([minSuff, maxSuff])
-					  .range([colorSet[0], colorSet[13]]);
+    // Scale color set based on max and min sufficiency scores
+        var minSuff = d3.min(countries, function (d) { return d.suff });
+        var maxSuff = d3.max(countries, function (d) { return d.suff });
+        var color = d3.scaleLinear()
+              .domain([minSuff, maxSuff])
+              .range([colorSet[0], colorSet[13]]);
 
     // Helper function: Update view function if a country is clicked
-    var updateView = function (countries, neighbors, selectedCountry) {
-    if (selectedCountry == null) {
-    	document.getElementById("header").innerHTML = "Food Self-Sufficiency Worldwide";
+      var updateView = function (countries, neighbors, selectedCountry) {
+      if (selectedCountry == null) {
+        document.getElementById("header").innerHTML = "Food Self-Sufficiency Worldwide";
+        svg.selectAll('.country')
+          .data(countries)
+          .attr('class', 'country')
+          .attr('d', path)
+          .style('fill', function(d, i) { return color(d.color = d.id); })
+          .style('opacity', 1.0)
+          .style('stroke', '#fff')
+          .on("mouseover", function () {
+            this.parentNode.appendChild(this);
+            d3.select(this)
+            .style('stroke', '#000');
+          })
+          .on("mouseout", function() {
+            d3.select(this)
+            .style('stroke', '#fff');
+          })
+          .on("click", function(d) {
+            updateView(countries, neighbors, d);
+          });
+          document.getElementById("legend")
+           .innerHTML = ("");
+          document.getElementById("legend")
+           .style.border = "0px";  
+      } 
+      else {
+        document.getElementById("header").innerHTML = "Food Self-Sufficiency in " + selectedCountry.name;
+        svg.selectAll('.country')
+          .data(countries)
+          .attr('class', 'country')
+          .attr('d', path)
+          .style('stroke', '#fff')
+          .style('opacity', function(d) { if (d.id != selectedCountry.id)
+            return 0.5;
+          })
+          .on("mouseover", function () {})
+          .on("mouseout", function () {})
+          .on("click", function(d) {
+            updateView(countries, neighbors, null);
+          });
+          var legendText = getlegendText(selectedCountry, sample);
+          console.log(legendText)
+          console.log(selectedCountry);
+        document.getElementById("legend")
+          .append(legendText);
+        document.getElementById("legend")
+          .style.border = "solid #000000";      
+        }
+      };
+
+      //Fill in countries by distinct colors
       svg.selectAll('.country')
-        .data(countries)
-        .attr('class', 'country')
-        .attr('d', path)
-        .style('fill', function(d, i) { return color(d.color = d.id); })
-        .style('opacity', 1.0)
-        .style('stroke', '#fff')
-        .on("mouseover", function () {
-          this.parentNode.appendChild(this);
-          d3.select(this)
-          .style('stroke', '#000');
-        })
-        .on("mouseout", function() {
-          d3.select(this)
-          .style('stroke', '#fff');
-        })
-        .on("click", function(d) {
-          updateView(countries, neighbors, d);
-        });
-        document.getElementById("key")
-         .innerHTML = ("");
-        document.getElementById("key")
-         .style.border = "0px";  
-    } 
-    else {
-    	document.getElementById("header").innerHTML = "Food Self-Sufficiency in " + selectedCountry.name;
-      svg.selectAll('.country')
-        .data(countries)
-        .attr('class', 'country')
-        .attr('d', path)
-        .style('stroke', '#fff')
-        .style('opacity', function(d) { if (d.id != selectedCountry.id)
-        	return 0.5;
-        })
-        .on("mouseover", function () {})
-        .on("mouseout", function () {})
-        .on("click", function(d) {
-          updateView(countries, neighbors, null);
-        });
-        var keyText = getKeyText(selectedCountry, sample);
-        console.log(keyText)
-        console.log(selectedCountry);
-      document.getElementById("key")
-        .append(keyText);
-      document.getElementById("key")
-        .style.border = "solid #000000";      
-      }
-    };
+              .data(countries)
+              .enter()
+              .append('path')
+                .attr('class', 'country')
+                .attr('d', path);
 
-		//Fill in countries by distinct colors
-		svg.selectAll('.country')
-		      	.data(countries)
-		      	.enter()
-		      	.append('path')
-		      		.attr('class', 'country')
-		      		.attr('d', path);
-
-		updateView(countries, neighbors, null);
-
-		//d3.slider().axis(true).min(1961).max(2013);
-       });// end d3.json
-  }); // end d3.tsv
-}); // end d3.csv
+      updateView(countries, neighbors, null);
+  }); // end d3.csv
+});// end d3.json
+}); // end d3.tsv
 
 /*  ----------------------
     ---HELPER FUNCTIONS---
@@ -239,7 +242,7 @@ function find_legend_data(name, data)
 }
 
 // Helper function to generate legend text
-function getKeyText(country, data) {
+function getlegendText(country, data) {
   if (country.suff == 0)
     var str = "No data for " + country.name;
   else {
